d28f897dda976a512ed6b94a21937217cc23eb80
MarynaLapa <133876007+MarynaLapa@users.noreply.github.com>
2023-09-16 20:12:34 +0300

    4 task

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,28 @@
 // Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
 
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+
+    let counterValue = 0;
+
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+    const buttonDecrement = document.querySelector('[data-action="decrement"]');
+    console.log(buttonDecrement);
+    const declineClick = () => {
+        counterValue -= 1;
+        console.log(counterValue);
+    }
+
+
+    const buttonIncrement = document.querySelector('[data-action="increment"]');
+    console.log(buttonIncrement);
+    const enlargeClick = () => {
+        counterValue += 1;
+        console.log(counterValue);
+    }
+
+    buttonIncrement.addEventListener("click", enlargeClick);
+    buttonDecrement.addEventListener("click", declineClick);
+
 
--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,13 @@
 // Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+
+const inputEl = document.querySelector('#name-input');
+console.log(inputEl);
+
+inputEl.addEventListener('keydown', event => {
+    console.log(event)
+    const a = event
+    if (a !== "") {
+    console.log('ff')
+}
+})
