5b91c6fe337fad3bf69d0e240ff0633f4c47321a
Lapamaryna <lapamaryna@i.ua>
2023-10-10 13:49:34 +0300

    10.10.2023

--- js/task-08.js
+++ js/task-08.js
@@ -5,19 +5,34 @@
 
     form.addEventListener('submit', handlerSumbit);
 
-// Під час відправлення форми сторінка не повинна перезавантажуватися.  !event.preventDefault();
-
 function handlerSumbit(event) {
+
+// Під час відправлення форми сторінка не повинна перезавантажуватися.  !event.preventDefault();
     event.preventDefault();
+
     console.dir(event.currentTarget);
+    
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
     const { email, password } = event.currentTarget;
     console.log(email);
     console.log(password);
     
-    }
+    if (email.value === "" || password.value === "") {
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
-// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
-// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+        alert('All fields must be filled.')
+    } else {
+        const data = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(data);
+
+    // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+    event.currentTarget.reset();
+    }
+}
+
 
 
 
--- js/task-09.js
+++ js/task-09.js
@@ -7,3 +7,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+console.log(getRandomHexColor())
+
+const changeColor = document.querySelector('.change-color');
+const bodyColor = document.querySelector('body');
+const colorValue = document.querySelector('.color');
+
+changeColor.addEventListener('click', handlerChangeColor)
+
+function handlerChangeColor() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+}
